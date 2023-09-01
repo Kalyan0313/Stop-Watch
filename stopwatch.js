67f70126334fa3d9ebd41a968e091d93f70e3307
Kalyan0313 <97174221+Kalyan0313@users.noreply.github.com>
2023-09-01 19:21:59 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,39 @@
+const clock=document.querySelector(".clock");
+let sec=0,min=0,hr=0,intervalId;
+
+
+const startStopwatch=()=>{
+intervalId= setInterval(()=>{
+    if(sec<59)
+    {
+        sec++;
+    }
+    else if(min>59)
+    {
+        min=0;
+        hr++;
+    }
+    else{
+        sec=0;
+        min++;
+    }
+
+    const seconds=String(sec).padStart(2,"0");
+    const minutes=String(min).padStart(2,"0");
+    const hours=String(hr).padStart(2,"0");
+
+
+    clock.innerText=`${hours}:${minutes}:${seconds}`;
+
+},1000);
+};
+
+const stopStopwatch=() =>{
+    clearInterval(intervalId);
+};
+
+const resetStopwatch=() =>{
+    clearInterval(intervalId);
+    sec=0;
+    clock.innerText='00:00:00';
+};
